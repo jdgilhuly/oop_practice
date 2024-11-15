9b6a92c143a40658610c2053b71458dba2ab672f
jdgilhuly <jdgilhuly@icloud.com>
2024-11-14 23:37:05 -0500

    starting ame

--- connect_four.py
+++ connect_four.py
@@ -1,3 +1,9 @@
-class Game():
+from abc import ABC, abstract method
+
+class Game(ABC):
 	def __init__(self):
-`
+		self.game = None
+
+	@abstractmethod
+	def create_game(self):
+		self.game = ConnectFour()
