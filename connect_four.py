f2b2f9381b50f739b79a8c2ec0bd4abf9d621054
jdgilhuly <jdgilhuly@icloud.com>
2024-11-14 23:39:19 -0500

    adding grid

--- connect_four.py
+++ connect_four.py
@@ -1,9 +1,16 @@
 from abc import ABC, abstract method
 
-class Game(ABC):
+class Game:
 	def __init__(self):
 		self.game = None
 
 	@abstractmethod
 	def create_game(self):
 		self.game = ConnectFour()
+
+class Grid():
+	def __init__(self, rows, columns):
+		self.rows = rows
+		self.columns = columns
+	
+	
