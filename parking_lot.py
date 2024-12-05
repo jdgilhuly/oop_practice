387d3ef9c1721188971431088e86e983ea09055d
jdgilhuly <jdgilhuly@icloud.com>
2024-12-04 22:38:02 -0800

    fixing parking lot and spaces'

--- parking_lot.py
+++ parking_lot.py
@@ -3,12 +3,16 @@ from abc import ABC, abstractmethod
 from enum import Enum
 
 class ParkingLot:
+
 	def __init__(self):
 		pass
+
+	def create_parking_lot(self, size):
+		self.parking_spaces = [ParkingSpace(size) for _ in range(size)]
+
 	def get_empty_parking_spaces(self):
 		pass
-	def park_car(self, car):
-		pass
+
 
 class ParkingSpace:
 
@@ -19,23 +23,31 @@ class ParkingSpace:
 	def get_parking_space_details(self):
 		return self.size
 
-	def park_car(self):
-		self.is_empty = False
+	def park_car(self, car_size):
+		if self.is_empty:
+			if self.size >= car_size:
+				self.is_empty = False
+				return True
+			else:
+				return False
+		else:
+			return False
 
 	def unpark_car(self):
 		self.is_empty = True
 
 class Car:
-	def __init__(self, type):
-		self.type = None
+	def __init__(self, type, color):
+		self.type = type
+		self.color = color
 
 	def get_car_details(self):
-		return self.type
+		return (self.type, self.color)
 
 class CarType(Enum):
 	SMALL = 1
 	MEDIUM = 2
 	LARGE = 3
 
-x = Car(CarType.SMALL)
+x = Car(CarType.SMALL, "red")
 print(x.get_car_details())
