b7065dd4cc0e46a520ed132b11dc6d3d81aec13f
jdgilhuly <jdgilhuly@icloud.com>
2024-12-04 22:28:33 -0800

    init

--- parking_lot.py
+++ parking_lot.py
@@ -1,5 +1,6 @@
 # parking_lot.py
-
+from abc import ABC, abstractmethod
+from enum import Enum
 
 class ParkingLot:
 	def __init__(self):
@@ -8,4 +9,11 @@ class ParkingSpace:
 	def __init__(self):
 
 class Car:
+	def __init__(self, type):
+		self.type = None
+		self.size = None
 
+class CarType(Enum):
+	SMALL = 1
+	MEDIUM = 2
+	LARGE = 3
