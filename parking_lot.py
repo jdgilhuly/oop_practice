e56ab18115b82467e1c4835fa91f7c600b04da24
jdgilhuly <jdgilhuly@icloud.com>
2024-12-05 10:30:39 -0800

    parking functionality

--- parking_lot.py
+++ parking_lot.py
@@ -21,20 +21,24 @@ class ParkingLot:
 		for _ in range(large_spaces):
 			self.parking_spaces[CarType.LARGE].append(ParkingSpace(CarType.LARGE))
 
-	def return_number_of_parking_spaces(self, size):
-		return len(self.parking_spaces[size])
-
-	def get_open_parking_spot(self, size):
+	def __get_open_parking_spot(self, size):
 		if not self.parking_spaces[size][0].is_empty:
 			return None
 		else:
 			return self.parking_spaces[size].popleft()
 
+	def return_number_of_parking_spaces(self, size):
+		return len(self.parking_spaces[size])
+
 	def park(self, car):
-		open_space = self.get_open_parking_spot(car.type)
-		if open_space:
-			if open_space.park_car(car.type):
+		parking_space = self.__get_open_parking_spot(car.type)
+		if parking_space:
+			if parking_space.park_car(car.type):
+				self.parking_spaces[car.type].append(parking_space)
 				return True
+			else:
+				self.parking_spaces[car.type].appendleft(parking_space)
+				return False
 		return False
 
 
@@ -52,10 +56,7 @@ class ParkingSpace:
 			if self.size >= car_size:
 				self.is_empty = False
 				return True
-			else:
-				return False
-		else:
-			return False
+		return False
 
 	def unpark_car(self):
 		self.is_empty = True
@@ -73,13 +74,14 @@ class CarType(Enum):
 	MEDIUM = 2
 	LARGE = 3
 
-x = Car(CarType.SMALL, "red")
-print(x.get_car_details())
+small_car = Car(CarType.SMALL, "red")
+print(small_car.get_car_details())
 
 
 parking_lot = ParkingLot(2, 2, 2)
-print(parking_lot.get_parking_lot_details())
 
-small_car = Car(CarType.SMALL, "red")
-parking_lot.park_car(small_car)
-print(parking_lot.get_parking_lot_details())
+print(parking_lot.return_number_of_parking_spaces(CarType.SMALL))
+parking_lot.park(small_car)
+print(parking_lot.return_number_of_parking_spaces(CarType.SMALL))
+
+
