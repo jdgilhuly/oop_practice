925158d2712f2e6118a13d0afb1ccb303189fe1d
jdgilhuly <jdgilhuly@icloud.com>
2024-12-05 11:32:30 -0800

    refactor

--- parking_lot.py
+++ parking_lot.py
@@ -3,29 +3,43 @@ from abc import ABC, abstractmethod
 from enum import Enum
 from collections import deque
 
+
 class ParkingLot:
 
 	def __init__(self, small_spaces, medium_spaces, large_spaces):
-		self.parking_spaces = {CarType.SMALL: deque(),
-							   CarType.MEDIUM: deque(),
-							   CarType.LARGE: deque()}
-		self.__create_parking_lot(small_spaces, medium_spaces, large_spaces)
+		self.parking_spaces = {}
+		self.spaces_available = {
+			CarType.SMALL.value: small_spaces,
+			CarType.MEDIUM.value: medium_spaces,
+			CarType.LARGE.value: large_spaces
+		}
 
+		self.__create_parking_lot(small_spaces, medium_spaces, large_spaces)
 
 	def __create_parking_lot(self, small_spaces, medium_spaces, large_spaces):
 
+		space_number = 0
+
 		for _ in range(small_spaces):
-			self.parking_spaces[CarType.SMALL].append(ParkingSpace(CarType.SMALL))
+			self.parking_spaces[space_number] = ParkingSpace(CarType.SMALL.value)
+			space_number += 1
+
 		for _ in range(medium_spaces):
-			self.parking_spaces[CarType.MEDIUM].append(ParkingSpace(CarType.MEDIUM))
+			self.parking_spaces[space_number] = ParkingSpace(CarType.SMALL.value)
+			space_number += 1
+
 		for _ in range(large_spaces):
-			self.parking_spaces[CarType.LARGE].append(ParkingSpace(CarType.LARGE))
+			self.parking_spaces[space_number] = ParkingSpace(CarType.SMALL.value)
+			space_number += 1
 
 	def __get_open_parking_spot(self, size):
 		if not self.parking_spaces[size][0].is_empty:
 			return None
 		else:
 			return self.parking_spaces[size].popleft()
+
+	def __get_car_type_from_ticket(self, ticket_number):
+		return self.spaces[ticket_number]
 
 	def return_number_of_parking_spaces(self, size):
 		return len(self.parking_spaces[size])
@@ -40,6 +54,13 @@ class ParkingLot:
 				self.parking_spaces[car.type].appendleft(parking_space)
 				return False
 		return False
+
+	def unpark(self, ticket_number):
+		car_type = self.__get_car_type_from_ticket(ticket_number)
+		parking_space = self.parking_spaces[car_type].popleft()
+		parking_space.unpark_car()
+		self.parking_spaces[car_type].append(parking_space)
+
 
 
 class ParkingSpace:
@@ -63,7 +84,7 @@ class ParkingSpace:
 
 class Car:
 	def __init__(self, type, color):
-		self.type = type
+		self.type = type.value
 		self.color = color
 
 	def get_car_details(self):
@@ -74,14 +95,18 @@ class CarType(Enum):
 	MEDIUM = 2
 	LARGE = 3
 
+
+# Init Cars
 small_car = Car(CarType.SMALL, "red")
 print(small_car.get_car_details())
 
-
+# Init Lot
 parking_lot = ParkingLot(2, 2, 2)
 
-print(parking_lot.return_number_of_parking_spaces(CarType.SMALL))
+
+# Test Parking
+print(parking_lot.return_number_of_parking_spaces(CarType.SMALL.value))
 parking_lot.park(small_car)
-print(parking_lot.return_number_of_parking_spaces(CarType.SMALL))
+print(parking_lot.return_number_of_parking_spaces(CarType.SMALL.value))
 
 
