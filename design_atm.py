f1ac7683133c81ea0092ee0b723e450cdc1ec492
jdgilhuly <jdgilhuly@icloud.com>
2024-12-06 14:53:21 -0800

    atm practice

--- design_atm.py
+++ design_atm.py
@@ -3,31 +3,51 @@
 # multiple account --> checking, saving with different rate of return
 
 from abc import ABC, abstractmethod
+from collections import defaultdict
 
 class ATM:
     def __init__(self):
-        self.accounts = {}
+        self.accounts = defaultdict(list)
+        self.user_uuid = None
 
-    def open_account(self):
+    def login(self, user_uuid):
+        self.user_uuid = user_uuid
+
+    def logout(self):
+        self.user_uuid = None
+
+    def open_account(self, type):
+        if self.user_uuid not in self.accounts:
+            self.accounts[self.user_uuid] = []
+        if type == "Checkings":
+            self.accounts[self.user_uuid].append(CheckingsAccount)
+        elif type == "Savings":
+            self.accounts[self.user_uuid].append(SavingsAccount)
+
+    def display_accounts(self):
+        return self.accounts[self.user_uuid]
+
+
+
 
 
 
 class Account(ABC):
     def __init__(self):
-        self.account_balance = 0
+        self.__account_balance = 0
 
     def get_balance(self):
-        return self.account_balance
+        return self.__account_balance
 
     def deposit_money(self, money):
-        self.account_balance += money
-        return self.account_balance
+        self.__account_balance += money
+        return self.__account_balance
 
     def withdraw_money(self, money):
         if self.get_balance - money < 0:
             return False
         else:
-            self.account_balance -= money
+            self.__account_balance -= money
             return True
 
     @abstractmethod
@@ -48,7 +68,5 @@ class SavingsAccount(Account):
     def savings_rate(self):
         return 0.05
 
-
-class User():
-    pass
+atm = ATM()
 
