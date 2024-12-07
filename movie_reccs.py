e63f08efc0309547bc8761d690123e5969fa4316
jdgilhuly <jdgilhuly@icloud.com>
2024-12-06 16:35:24 -0800

    first pass

--- movie_reccs.py
+++ movie_reccs.py
@@ -9,14 +9,8 @@
 
 # design movies
 
+import heapq
 
-
-class User:
-    # Iteracts with Rec System and Creates Recommendation Scores
-    pass
-
-class RecommendationSystem:
-    pass
 
 import enum
 class Genre(enum.Enum):
@@ -32,16 +26,47 @@ class Movie:
         self.movie_actors = movie_actors
         self.movie_genre = movie_genre
 
+    def get_movie_genre(self):
+        return self.movie_genre
+
     def get_movie_name(self):
         return self.movie_name
 
+    def get_movie_actors(self):
+        return self.movie_actors
 
-
-
-class Feed:
-    # Returns Ordered Recommendations
-    pass
 
 class Recommendation(Movie):
     # Takes in Movie and has recommendation score attached
-    pass
+    def __init__(self, rating, name, actors, genre):
+        super().__init__(name, actors, genre)
+        self.rating = [rating]
+
+    def get_movie_rating(self):
+        return sum(self.rating) // len(self.rating)
+
+    def add_movie_rating(self, new_rating):
+        self.rating.append(new_rating)
+        return sum(self.rating) // len(self.rating)
+
+
+class RecommendationSystem:
+    def __init__(self, movies):
+        self.recommendations = self.__get_most_recent_recc(movies)
+
+    def __get_most_recent_recc(self, movies):
+        recs = []
+        for movie in movies:
+            heapq.heappush((-movie.get_movie_rating(), movie))
+        return recs
+
+    def return_reccomendation(self):
+        return self.recommendations
+
+
+class Feed:
+    def __init__(self):
+        self.rec_system = RecommendationSystem()
+
+    def show_reccs(self):
+        print(self.rec_system.return_reccomendation())
