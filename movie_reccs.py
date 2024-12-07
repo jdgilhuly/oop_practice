1ad30011445a65c9e0a54a608f6d6728c2188a4a
jdgilhuly <jdgilhuly@icloud.com>
2024-12-06 16:21:29 -0800

    ordering

--- movie_reccs.py
+++ movie_reccs.py
@@ -12,16 +12,36 @@
 
 
 class User:
+    # Iteracts with Rec System and Creates Recommendation Scores
     pass
 
 class RecommendationSystem:
     pass
 
+import enum
+class Genre(enum.Enum):
+    HORROR = 1
+    COMEDY = 2
+    THRILLER = 3
+    # ... add more
+
 class Movie:
-    pass
+    # instance of movie with relevant info
+    def __init__(self, movie_name, movie_actors, movie_genre):
+        self.movie_name = movie_name
+        self.movie_actors = movie_actors
+        self.movie_genre = movie_genre
+
+    def get_movie_name(self):
+        return self.movie_name
+
+
+
 
 class Feed:
+    # Returns Ordered Recommendations
     pass
 
-class Recommendation:
+class Recommendation(Movie):
+    # Takes in Movie and has recommendation score attached
     pass
