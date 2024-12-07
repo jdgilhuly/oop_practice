4057f09d0cecb09d63dc87b17d551f572d497a41
jdgilhuly <jdgilhuly@icloud.com>
2024-12-06 16:15:47 -0800

    init

---
+++ movie_reccs.py
@@ -0,0 +1,15 @@
+# build a movie reccomendation system
+# users
+    # rate movies
+    # get reccs
+
+# recommendation system
+    # return reccs
+    # get reccs for users
+
+# design movies
+
+
+
+class User:
+    pass
