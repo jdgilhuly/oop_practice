f2c5f0f64908779aeb0b89f8492caa84ad7fad82
jdgilhuly <jdgilhuly@icloud.com>
2024-12-10 18:19:13 -0800

    init

--- parking_lot.py
+++ parking_lot.py
@@ -5,3 +5,33 @@
 # ticketpricingstrategy --> tickets machine
 
 # tickets correspond with spaces
+
+class Car:
+    def __init__(self, type):
+        self.type =
+    pass
+
+import enum
+class CarSizes(enum.Enum):
+    SMALL = 1
+    MEDIUM = 2
+    BIG = 3
+
+
+
+class Space:
+    pass
+
+class ParkingLot:
+    pass
+
+class Ticket:
+    pass
+
+class TicketMachine:
+    pass
+
+from abc import ABC, abstractmethod
+class TicketPricingStrategy(ABC):
+    pass
+
